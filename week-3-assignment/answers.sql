443477334842a79f27849bb761430b6583b18d7c
crispusomollo <crispusomollo@gmail.com>
2024-09-19 17:10:06 +0300

    updated question 1.1

--- week-3-assignment/answers.sql
+++ week-3-assignment/answers.sql
@@ -1,7 +1,10 @@
 -- Part 1: Basic Aggregate Functions
 
 -- question 1.1
-SELECT COUNT(*) AS total_admissions FROM admissions;
+SELECT 
+    COUNT(*) AS total_admissions
+FROM
+    admissions;
 
 -- question 1.2
 SELECT 
