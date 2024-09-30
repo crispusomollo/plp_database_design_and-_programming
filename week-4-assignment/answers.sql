f533246a43092abbf64fd0319260d02d01919195
crispusomollo <crispusomollo@gmail.com>
2024-09-30 16:52:25 +0300

    created the answers file

---
+++ week-4-assignment/answers.sql
@@ -0,0 +1,7 @@
+-- PART 1: INSERT DATA
+
+-- question 1.1
+INSERT INTO patients (first_name, last_name, date_of_birth, gender, language)
+VALUES ('John', 'Doe', '1980-11-15', 'Male', 'English');
+
+
