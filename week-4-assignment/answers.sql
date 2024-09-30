cda9107da9b135f0026d60e1b6192d1ecf857941
crispusomollo <crispusomollo@gmail.com>
2024-09-30 17:00:31 +0300

    answer to question 2

--- week-4-assignment/answers.sql
+++ week-4-assignment/answers.sql
@@ -1,6 +1,13 @@
--- PART 1: INSERT DATA
+-- Part 1: INSERT Data
+
 -- question 1.1
 INSERT INTO patients (first_name, last_name, date_of_birth, gender, language)
 VALUES ('John', 'Doe', '1980-11-15', 'Male', 'English');
 
 
+-- Part 2: UPDATE Data
+
+-- question 2.1
+UPDATE patients
+SET language = 'Spanish'
+WHERE first_name = 'John' AND last_name = 'Doe';
